a96c2030713375026da9f9bf8cf99ee02da9c96c
soichiro83838 <sli224quwnfj@gmail.com>
2021-01-10 22:42:23 +0900

    SPRING TEST初回のコミット

---
+++ testdemo/src/main/java/testcom/example/testdemo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package testcom.example.testdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+}
